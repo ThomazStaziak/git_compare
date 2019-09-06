e35ed2f8b843db45630f841e3384a62d6ae3e1fb
ThomazStaziak <thostaziak@gmail.com>
2019-09-06 16:51:26 -0300

    chore: adding the Repository component

---
+++ src/pages/Repository/index.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import api from '../../services/api';
+
+import Container from '../../components/Container';
+import { Loading, Owner, IssueList } from './styles';
+
+export default function Repository({ match }) {
+  const [repository, setRepository] = useState({});
+  const [issues, setIssues] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(async () => {
+    const repoName = decodeURIComponent(match.params.repository);
+
+    const [repoDetails, issuesData] = await Promise.all([
+      api.get(`/repos/${repoName}`),
+      api.get(`/repos/${repoName}/issues`, {
+        params: {
+          state: 'open',
+          per_page: 5,
+        },
+      }),
+    ]);
+
+    setRepository(repoDetails.data);
+    setIssues(issuesData.data);
+    setLoading(false);
+  }, [match.params.repository]);
+
+  if (loading) {
+    return <Loading>Loading...</Loading>;
+  }
+
+  return (
+    <Container>
+      <Owner>
+        <Link to="/">Back to the repositories</Link>
+        <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+        <h1>{repository.name}</h1>
+        <p>{repository.description}</p>
+      </Owner>
+
+      <IssueList>
+        {issues.map(issue => (
+          <li key={String(issue.id)}>
+            <img src={issue.user.avatar_url} alt={issue.user.login} />
+            <div>
+              <strong>
+                <a href={issue.html_url}>{issue.title}</a>
+                {issue.labels.map(label => (
+                  <span key={String(label.id)}>{label.name}</span>
+                ))}
+              </strong>
+              <p>{issue.user.login}</p>
+            </div>
+          </li>
+        ))}
+      </IssueList>
+    </Container>
+  );
+}
+
+Repository.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      repository: PropTypes.string,
+    }),
+  }).isRequired,
+};
