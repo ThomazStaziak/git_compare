1fa7e2c51b01e3b00c7ac8306463e5421bf0f356
ThomazStaziak <thostaziak@gmail.com>
2019-09-06 17:08:05 -0300

    Updates

---
+++ precache-manifest.beffb13c9c7a616661e05f597fd6456d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8caf95762477b03d9d253d60ea9d682a",
+    "url": "/git_compare/index.html"
+  },
+  {
+    "revision": "30b16602be429703e76f",
+    "url": "/git_compare/static/js/2.07fb7dae.chunk.js"
+  },
+  {
+    "revision": "20148e9872052c1b61be",
+    "url": "/git_compare/static/js/main.801453db.chunk.js"
+  },
+  {
+    "revision": "d4493faddc83ff00d680",
+    "url": "/git_compare/static/js/runtime~main.1cd10923.js"
+  }
+]);
